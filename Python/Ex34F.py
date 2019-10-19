327a67abd1bb9657d50a78a51f9f3fb80017c4a0
Joseffe Oliveira <53655211+Joseffe10@users.noreply.github.com>
2019-10-19 11:10:24 -0300

    Add files via upload

---
+++ Python/Ex34F.py
@@ -0,0 +1,5 @@
+print('Tabuada com FOR')
+num = int(input('Digite um número: '))
+for i in range(1, 11, 1):
+    r = num * i
+    print(f'{num} X {i} = {r}')
---
+++ Python/Ex34W.py
@@ -0,0 +1,7 @@
+print('Tabuada com WHILE')
+num = int(input('Digite um número: '))
+i = 1
+while(i < 11):
+    r = num * i
+    print(f'{num} X {i} = {r}')
+    i = i + 1
