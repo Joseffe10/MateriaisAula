497474304267d38542946efc64d8859d329d16d0
Joseffe Oliveira <53655211+Joseffe10@users.noreply.github.com>
2019-10-22 20:48:58 -0300

    Add files via upload

--- Banco de Dados/Script Tabelas.sql
+++ Banco de Dados/Script Tabelas.sql
@@ -1,16 +1,17 @@
+-- Criação do Banco de Dados
+create database db_esamc
+
+-- Utiliza o Banco de Dados criado
 use db_esamc
 
 -- Criação de Tabela
 create table tb_curso
 (
-	id_curso int auto_increment not null
+	id_curso int not null
    ,nm_curso varchar(50) not null
    ,nota_mec float null
    ,primary key (id_curso)
 )
-
--- Exclusão de Tabela
-drop table tb_curso
 
 -- Inclusão de Registro na Tabela
 insert into tb_curso (id_curso, nm_curso, nota_mec) values (3, 'PETROLEO E GAZ', null)
@@ -18,8 +19,6 @@ insert into tb_curso (id_curso, nm_curso, nota_mec) values (3, 'PETROLEO E GAZ',
 -- Exclusão de Registro na Tabela
 delete from tb_curso where id_curso = 2
 
--- Apresenta todas as tabelas do Banco de Dados
-show tables
-
+-- Apresenta os registros que estão na Tabela
 select *
 from tb_curso
